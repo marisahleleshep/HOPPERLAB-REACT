0e7c15e7b23c7e1cdc642204f07b962561d66f89
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-06-22 08:12:53 +0300

    first commit

---
+++ src/Introduction/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+const Introduction=()=>{
+    return(
+        <div>
+            <h1>What I learnt in React</h1>
+            <p>It is a JS library development by FaceBook</p>
+            <p>It has reusable components</p>
+        </div>
+    )
+}
+export default Introduction;
